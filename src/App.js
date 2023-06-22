06ad08fbee68a5767a053cf6b27b27f149044d2d
Ivan Chew <120282621+trickstyle89@users.noreply.github.com>
2023-06-22 15:34:54 -0300

    firstapp

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from "react"
+import "style.css"
+import "tailwindcss/lib/css/preflight.css"
+import AnimationRevealPage from "helpers/AnimationRevealPage"
+import Hero from "components/hero/FullWidthWithImage"
+import Pitch from "components/features/TwoColWithButton"
+import Skills from "components/features/ThreeColWithSideImageWithPrimaryBackground"
+import Portfolio from "components/features/VerticalWithAlternateImageAndText"
+import AboutMe from "components/features/TwoColSingleFeatureWithStats"
+import Footer from "components/footers/MiniCenteredFooter"
+
+
+function App() {
+  return (
+    <AnimationRevealPage>
+      <Hero />
+      <Pitch />
+      <Portfolio />
+      <AboutMe />
+      <Skills />
+      <Footer />
+    </AnimationRevealPage>
+  )
+}
+
+export default App
