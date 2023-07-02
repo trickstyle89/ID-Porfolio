f00c0b9c70e9aa3a4dfcf9d2fde7d4decea460ad
Ivan Chew <120282621+trickstyle89@users.noreply.github.com>
2023-07-02 15:31:10 -0300

    Revert "Reconfigured Components"
    This reverts commit 7e35b39089fcf162cbdb3e73c4345569ad7c45a2.

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import "style.css"
 import "tailwindcss/lib/css/preflight.css"
 import AnimationRevealPage from "helpers/AnimationRevealPage"
 import Hero from "components/hero/FullWidthWithImage"
-import Skillset from "components/features/ThreeColWithSideImage"
-import Portfolio from "components/blogs/ThreeColSimpleWithImage"
+import Pitch from "components/features/TwoColWithButton"
+import Skills from "components/features/ThreeColWithSideImage"
+import Portfolio from "components/features/VerticalWithAlternateImageAndText"
 import AboutMe from "components/features/TwoColSingleFeatureWithStats"
 import Footer from "components/footers/MiniCenteredFooter"
 
@@ -13,9 +14,10 @@ function App() {
   return (
     <AnimationRevealPage>
       <Hero />
-      <AboutMe />
-      <Skillset />
+      <Pitch />
       <Portfolio />
+      <AboutMe />
+      <Skills />
       <Footer />
     </AnimationRevealPage>
   )
